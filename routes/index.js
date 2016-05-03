c7415780f6c9d1ea99b25ba5a6971924d14e41fe
marktao <marktao1995@qq.com>
2016-05-03 20:51:26 +0800

    create NodeBlog

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+module.exports = function(app) {
+    app.get('/',function(req,res) {
+        res.render('index',{title:'Mark Express'});
+    });
+
+    app.render()
+};
